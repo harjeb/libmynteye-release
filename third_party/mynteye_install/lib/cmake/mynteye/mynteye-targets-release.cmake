3e45f7da93e1cb290d596ada93924cbb1a4debcb
harjeb <harjeb@outlook.com>
2019-11-13 16:27:55 +0800

    Rebase from 'upstream'

--- third_party/mynteye_install/lib/cmake/mynteye/mynteye-targets-release.cmake
+++ third_party/mynteye_install/lib/cmake/mynteye/mynteye-targets-release.cmake
@@ -8,12 +8,12 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 # Import target "mynteye" for configuration "Release"
 set_property(TARGET mynteye APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(mynteye PROPERTIES
-  IMPORTED_LOCATION_RELEASE "/home/mynt/MYNT-EYE-S-SDK/_install/lib/libmynteye.so.2.5.0"
+  IMPORTED_LOCATION_RELEASE "${MYNTEYES_SDK_ROOT}/lib/libmynteye.so.2.5.0"
   IMPORTED_SONAME_RELEASE "libmynteye.so.2"
   )
 
 list(APPEND _IMPORT_CHECK_TARGETS mynteye )
-list(APPEND _IMPORT_CHECK_FILES_FOR_mynteye "/home/mynt/MYNT-EYE-S-SDK/_install/lib/libmynteye.so.2.5.0" )
+list(APPEND _IMPORT_CHECK_FILES_FOR_mynteye "${MYNTEYES_SDK_ROOT}/lib/libmynteye.so.2.5.0" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
